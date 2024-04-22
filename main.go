2bd2ee1d52f47795082e291c70d974a90aafd3ae
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2024-04-22 17:13:07 -0300

    test: add unit test in app migrations

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-const migrationsDir = "./migrations"
+var MigrationsDir = "./migrations"
 
 // Configuração do banco de dados
 type Cfg struct {
@@ -79,7 +79,7 @@ func GenerateMigration(schemas ...Schema) (string, error) {
 	// - Cria um arquivo com o nome da migração no diretório de migrações.
 	timestamp := time.Now().Format("20060102150405")
 	migrationFileName := fmt.Sprintf("migration_%s.sql", timestamp)
-	file, err := os.Create(filepath.Join(migrationsDir, migrationFileName))
+	file, err := os.Create(filepath.Join(MigrationsDir, migrationFileName))
 	if err != nil {
 		return "", err
 	}
@@ -115,16 +115,16 @@ func GenerateMigration(schemas ...Schema) (string, error) {
 	return migrationFileName, nil
 }
 
-// RunMigrations executa todas as migrações encontradas no diretório migrationsDir no banco de dados especificado.
+// RunMigrations executa todas as migrações encontradas no diretório MigrationsDir no banco de dados especificado.
 // Retorna um possível erro, se houver.
 func RunMigrations(db *sql.DB) error {
 	// 1. Verificar se o diretório de migrações existe
-	if _, err := os.Stat(migrationsDir); os.IsNotExist(err) {
+	if _, err := os.Stat(MigrationsDir); os.IsNotExist(err) {
 		return fmt.Errorf("O diretório de migrações não existe")
 	}
 
 	// 2. Listar arquivos de migração
-	files, err := ioutil.ReadDir(migrationsDir)
+	files, err := ioutil.ReadDir(MigrationsDir)
 	if err != nil {
 		return fmt.Errorf("Erro ao listar arquivos de migração: %v", err)
 	}
@@ -132,7 +132,7 @@ func RunMigrations(db *sql.DB) error {
 	// 3. Executar migrações
 	for _, f := range files {
 		if !f.IsDir() && filepath.Ext(f.Name()) == ".sql" {
-			migrationPath := filepath.Join(migrationsDir, f.Name())
+			migrationPath := filepath.Join(MigrationsDir, f.Name())
 			fmt.Println("Executando migração:", migrationPath)
 
 			// Lê o conteúdo do arquivo de migração
